a74d79b23c1eb32b72da87da5e8ed00ba7b40f79
b-kwizera <b.kwizera@alustudent.com>
2025-06-19 15:27:29 +0200

    Update copilot_shell_script.sh
    changing the app folder path to the one i created earliersubmission_reminder_bodgar/config/config.env

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Asking user for the new assignment name
-read -p "Enter the new assignment name: " new_assignment
+# Asking the user for the new assignment name
+read -p "Enter new assignment name: " new_assignment
 
-# Updating the ASSIGNMENT value in config.env
-sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" ./config/config.env
+# Updating the ASSIGNMENT value in the config.env file inside the app folder
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" submission_reminder_bodgar/config/config.env
 
-# Runnig startup.sh to check who hasn't submitted
-bash ./scripts/startup.sh
+# Confirmation message
+echo "ASSIGNMENT updated to: $new_assignment"
